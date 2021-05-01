9c4bc2c2efecff3734aa101109e6161efc65da92
Zuhaitz <zuhaitzmartinezjimenez@gmail.com>
2021-05-01 14:50:19 +0200

    Week13 - Proyecto creado

---
+++ Week13-WebSockets/web_sockets/public/js/main.js
@@ -0,0 +1,39 @@
+import {loadSpritesheet, actualizar} from './pang.js';
+import {setupSockets} from "./sockets.js";
+
+const canvas = document.getElementById('pang');
+var x = 0;
+var y = 0;
+
+
+window.onload = function(){
+        loadSpritesheet();
+        setupSockets();
+        document.addEventListener('keydown', avisoKey);
+
+}
+
+
+function avisoKey (tecla) {
+        switch(tecla.code){
+                case 'ArrowUp':
+                        y>0 ? actualizar(x, --y) : null;
+                        tecla.preventDefault();
+                        break;
+                case 'ArrowDown':
+                        y<canvas.height ? actualizar(x, ++y) : null;
+                        tecla.preventDefault();
+                        break;
+                case 'ArrowLeft':
+                        x>0 ? actualizar(--x, y) : null;
+                        tecla.preventDefault();
+                        break;
+                case 'ArrowRight':
+                        x<canvas.height ? actualizar(++x, y) : null;
+                        tecla.preventDefault();
+                        break;
+                default: break;
+        }
+}
+
+//window.onload = setupSockets;
